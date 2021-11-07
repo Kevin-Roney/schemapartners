4970d04fffe354fde0fe78d4828b92194107d13d
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-11-06 21:16:36 -0700

    add environment variable to config file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    TO_EMAIL_ADDRESS: process.env.TO_EMAIL_ADDRESS,
+    FROM_EMAIL_ADDRESS: process.env.FROM_EMAIL_ADDRESS,
+  },
 }
