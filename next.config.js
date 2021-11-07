80700cab57952bcb1e5bc64e3934e38fef9203b7
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-11-06 21:27:05 -0700

    remove environment variable to config file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    TO_EMAIL_ADDRESS: process.env.TO_EMAIL_ADDRESS,
-    FROM_EMAIL_ADDRESS: process.env.FROM_EMAIL_ADDRESS,
-  },
 }
