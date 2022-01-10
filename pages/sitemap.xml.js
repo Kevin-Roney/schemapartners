80ff62d64017fe37f1385b9a19b8eae23b67efcd
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2022-01-09 19:52:02 -0800

    remove double slash in sitemap paths

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -3,8 +3,8 @@ const Sitemap = () => { };
 export const getServerSideProps = ({ res }) => {
 
     const baseUrl = {
-        development: "http://localhost:3000/",
-        production: "https://https://www.schemapartners.com/",
+        development: "http://localhost:3000",
+        production: "https://https://www.schemapartners.com",
     }[process.env.NODE_ENV];
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
@@ -16,19 +16,19 @@ export const getServerSideProps = ({ res }) => {
                 <priority>1.0</priority>
             </url>
             <url>
-                <loc>${baseUrl}Services</loc>
+                <loc>${baseUrl}/Services</loc>
                 <lastmod>${new Date().toISOString()}</lastmod>
                 <changefreq>monthly</changefreq>
                 <priority>1.0</priority>
             </url>
             <url>
-                <loc>${baseUrl}About</loc>
+                <loc>${baseUrl}/About</loc>
                 <lastmod>${new Date().toISOString()}</lastmod>
                 <changefreq>monthly</changefreq>
                 <priority>1.0</priority>
             </url>
             <url>
-                <loc>${baseUrl}Contact</loc>
+                <loc>${baseUrl}/Contact</loc>
                 <lastmod>${new Date().toISOString()}</lastmod>
                 <changefreq>monthly</changefreq>
                 <priority>1.0</priority>
