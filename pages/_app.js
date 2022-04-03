a1fdb5876304e77d1550702eadce5f589c136a00
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2022-04-03 14:42:58 -0700

    add google analytics to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Script from 'next/script'
+
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import styles from '../styles/globals.css'
@@ -7,6 +9,18 @@ import styles from '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <div className={styles.page} id='booger'>
+      <Script strategy="lazyOnload"
+        src={'https://www.googletagmanager.com/gtag/js?id=G-P4V5WSCDYP'}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-P4V5WSCDYP'); 
+       `}
+      </Script>
       <Head>
         <title>Schema Partners</title>
         <meta name="description" content="A Certified Value Analyst offering Mergers and Aquisitions Advising" />
