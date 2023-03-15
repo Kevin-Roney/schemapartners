d254ba988a84237869ef0a99ef8afc280c180109
Kevin Roney <ktroney.pdx@gmail.com>
2023-03-15 16:34:15 -0700

    added general styling to video iFrames and wrappers

--- components/YoutubeEmbed.js
+++ components/YoutubeEmbed.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from '../styles/Services.module.css'
 
 const YoutubeEmbed = ({ embedId }) => (
-  <div className="video-responsive">
+  <div className={styles.videoResponsive}>
     <iframe
-      width="853"
-      height="480"
+      width="380"
+      height="214"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
